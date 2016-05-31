8b2cd010d13098132a0e3e6d99fba4956a0169d6
alessiocoser <alessio.coser@xpeppers.com>
2016-05-31 15:11:12 +0200

    test refactoring

--- opening_hours_test.js
+++ opening_hours_test.js
@@ -10,28 +10,38 @@ const FRIDAY_AT_EIGHT          = new Date('2016-05-13T08:00:00.000Z');
 const NEXT_MONDAY_AT_EIGHT     = new Date('2016-05-16T08:00:00.000Z');
 
 describe('Is open on', function() {
+
   it('Shop is open on wednesday', function() {
     expect(shop.isOpenOn(WEDNESDAY)).to.eql(true);
   });
+
   it('Shop is not open on thursday', function() {
     expect(shop.isOpenOn(THURSDAY)).to.eql(false);
   });
+
   it('Shop is not open on wednesday until 7:59', function() {
     expect(shop.isOpenOn(WEDNESDAY_BEFORE_OPENING)).to.eql(false);
   });
+
   it('Shop is not open on wednesday from 16:00', function() {
     expect(shop.isOpenOn(WEDNESDAY_AFTER_CLOSING)).to.eql(false);
   });
-  it('On thursday the next opening date is Friday 13 May 2016 at 08:00', function() {
+
+});
+
+describe('Next Opening Day', function() {
+
+  it('On thursday is Friday 13 May 2016 at 08:00', function() {
     expect(shop.isOpenOn(THURSDAY)).to.eql(false);
     expect(shop.nextOpeningDate(THURSDAY)).to.eql(FRIDAY_AT_EIGHT);
   });
-  it('On wednesday the next opening date is Friday 13 May 2016 at 08:00', function() {
-    expect(shop.isOpenOn(WEDNESDAY)).to.eql(true);
+
+  it('On wednesday is Friday 13 May 2016 at 08:00', function() {
     expect(shop.nextOpeningDate(WEDNESDAY)).to.eql(FRIDAY_AT_EIGHT);
   });
-  it('On friday the next opening date is Monday 16 May 2016 at 08:00', function() {
-    expect(shop.isOpenOn(FRIDAY_AT_EIGHT)).to.eql(true);
+
+  it('On friday is next Monday 16 May 2016 at 08:00', function() {
     expect(shop.nextOpeningDate(WEDNESDAY)).to.eql(NEXT_MONDAY_AT_EIGHT);
   });
+
 });
