612a60d42b334716f855c12ed4a72249501de2e0
alessiocoser <alessio.coser@xpeppers.com>
2016-05-31 14:07:13 +0200

    add days configuration

---
+++ config.js
@@ -0,0 +1,13 @@
+const DAYS = {
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6,
+  SUNDAY: 7,
+};
+
+module.exports = {
+  "opening_days": [DAYS.MONDAY, DAYS.WEDNESDAY, DAYS.FRIDAY]
+}
