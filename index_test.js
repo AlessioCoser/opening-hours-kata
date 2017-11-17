7d11c738c73f19072cfc0a13fc8be9296dde5d0b
Alessio Coser <alessio.coser@xpeppers.com>
2017-11-17 14:55:59 +0100

    extracts DAYS constant

--- index_test.js
+++ index_test.js
@@ -1,6 +1,16 @@
 const { equal } = require('assert')
 const OpeningHours = require('./index')
 
+const DAYS = {
+  'sunday': 0,
+  'monday': 1,
+  'tuesday': 2,
+  'wednesday': 3,
+  'thursday': 4,
+  'friday': 5,
+  'saturday': 6
+}
+
 describe('OpeningHours', () => {
   let wednesday = new Date('2016-05-11T12:22:11.824Z')
   let thursday = new Date('2016-05-12T12:22:11.824Z')
@@ -15,7 +25,7 @@ describe('OpeningHours', () => {
   })
 
   describe('#IsOpenOn', () => {
-    let openDays = [1, 3, 5]
+    let openDays = [DAYS['monday'], DAYS['wednesday'], DAYS['friday']]
 
     it('Without day returns false', () => {
       let openingHours = new OpeningHours(openDays)
